ce94c8b2be0d338d85d94d2bb5e215cd6ac11bfd
umpyl <566015@edu.rocmn.nl>
2023-03-08 10:28:39 +0100

    challange 3 done

--- UmbrellaAcademy.Roster.Tests/MemberStoreTests.cs
+++ UmbrellaAcademy.Roster.Tests/MemberStoreTests.cs
@@ -13,113 +13,114 @@ namespace UmbrellaAcademy.Roster.Tests
 
         }
 
-        //[Fact]
-        //public void Get_Members_Should_Return_Correct_Number_Of_Members()
-        //{
-        //    _rosterStorage.ClearMembers();
+        [Fact]
+        public void Get_Members_Should_Return_Correct_Number_Of_Members()
+        {
+            _rosterStorage.ClearMembers();
 
-        //    var memberOne = new Member
-        //    {
-        //        Number = 1,
-        //        Name = "Frankie"
-        //    };
+            var memberOne = new Member
+            {
+                Number = 1,
+                Name = "Frankie"
+            };
 
-        //    var memberTwo = new Member
-        //    {
-        //        Number = 2,
-        //        Name = "Jackie"
-        //    };
+            var memberTwo = new Member
+            {
+                Number = 2,
+                Name = "Jackie"
+            };
 
-        //    _rosterStorage.AddMember(memberOne);
-        //    _rosterStorage.AddMember(memberTwo);
+            _rosterStorage.AddMember(memberOne);
+            _rosterStorage.AddMember(memberTwo);
 
-        //    var memberService = new MemberService(_rosterStorage);
-        //    var foundMembers = memberService.GetMembers(2, 0);
+            var memberService = new MemberService(_rosterStorage);
+            var foundMembers = memberService.GetMembers(2, 0);
 
-        //    Assert.Equal(memberOne, foundMembers.ElementAtOrDefault(0));
+            Assert.Equal(memberOne, foundMembers.ElementAtOrDefault(0));
 
-        //    Assert.Equal(memberTwo, foundMembers.ElementAtOrDefault(1));
+            Assert.Equal(memberTwo, foundMembers.ElementAtOrDefault(1));
 
-        //    Assert.Null(foundMembers.ElementAtOrDefault(2));
+            Assert.Null(foundMembers.ElementAtOrDefault(2));
 
-        //}
+        }
 
-        //[Fact]
-        //public void Get_Members_Should_Return_Correct_Number_Of_MembersOnPage()
-        //{
-        //    _rosterStorage.ClearMembers();
-        //
-        //    var memberOne = new Member
-        //    {
-        //        Number = 1,
-        //        Name = "Frankie"
-        //    };
+        [Fact]
+        public void Get_Members_Should_Return_Correct_Number_Of_MembersOnPage()
+        {
+            _rosterStorage.ClearMembers();
 
-        //    var memberTwo = new Member
-        //    {
-        //        Number = 2,
-        //        Name = "Jackie"
-        //    };
+            var memberOne = new Member
+            {
+                Number = 1,
+                Name = "Frankie"
+            };
 
-        //    _rosterStorage.AddMember(memberOne);
-        //    _rosterStorage.AddMember(memberTwo);
+            var memberTwo = new Member
+            {
+                Number = 2,
+                Name = "Jackie"
+            };
 
-        //    var memberService = new MemberService(_rosterStorage);
-        //    var foundMembers = memberService.GetMembers(1, 1);
+            _rosterStorage.AddMember(memberOne);
+            _rosterStorage.AddMember(memberTwo);
 
-        //    Assert.Equal(memberTwo, foundMembers.ElementAtOrDefault(0));
+            var memberService = new MemberService(_rosterStorage);
+            var foundMembers = memberService.GetMembers(1, 1);
 
-        //    Assert.Null(foundMembers.ElementAtOrDefault(1));
-        //}
+            Assert.Equal(memberTwo, foundMembers.ElementAtOrDefault(0));
 
-        
+            Assert.Null(foundMembers.ElementAtOrDefault(1));
+        }
 
-        //[Fact]
-        //public void Get_Member_By_Correct_Number_Should_Return_Correct_Member()
-        //{
-        //_rosterStorage.ClearMembers();
 
-        //var memberOne = new Member
-        //{
-        //    Number = 1,
-        //    Name = "Frankie"
-        //};
 
-        //var memberTwo = new Member
-        //{
-        //    Number = 2,
-        //    Name = "Jackie"
-        //};
+        [Fact]
+        public void Get_Member_By_Correct_Number_Should_Return_Correct_Member()
+        {
+            _rosterStorage.ClearMembers();
 
-        //_rosterStorage.AddMember(memberOne);
-        //_rosterStorage.AddMember(memberTwo);
+            var memberOne = new Member
+            {
+                Number = 1,
+                Name = "Frankie"
+            };
 
-        //// Write test
-        //var memberService = new MemberService(_rosterStorage);            
-        //}
+            var memberTwo = new Member
+            {
+                Number = 2,
+                Name = "Jackie"
+            };
 
-        //[Fact]
-        //public void Get_Member_By_Wrong_Number_Should_Return_No_Member()
-        //{
-        //_rosterStorage.ClearMembers();
+            _rosterStorage.AddMember(memberOne);
+            _rosterStorage.AddMember(memberTwo);
 
-        //var memberOne = new Member
-        //{
-        //    Number = 1,
-        //    Name = "Frankie"
-        //};
+            //Write test
+            var memberService = new MemberService(_rosterStorage);
+        }
 
-        //var memberTwo = new Member
-        //{
-        //    Number = 2,
-        //    Name = "Jackie"
-        //};
+        [Fact]
+        public void Get_Member_By_Wrong_Number_Should_Return_No_Member()
+        {
+            _rosterStorage.ClearMembers();
 
-        //_rosterStorage.AddMember(memberOne);
-        //_rosterStorage.AddMember(memberTwo);
+            var memberOne = new Member
+            {
+                Number = 1,
+                Name = "Frankie"
+            };
 
-        // Write test
-        //var memberService = new MemberService(_rosterStorage);
+            var memberTwo = new Member
+            {
+                Number = 2,
+                Name = "Jackie"
+            };
+
+            _rosterStorage.AddMember(memberOne);
+            _rosterStorage.AddMember(memberTwo);
+
+            //Write test;
+            var memberService = new MemberService(_rosterStorage);
+        }
     }
 }
 
